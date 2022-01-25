24ed4d37c90dc1a426616bf5a9ad189bac912db8
roswergf <6142356mn>
2022-01-25 14:17:22 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:band_names/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomePage(),
+      },
+    );
+  }
+}
